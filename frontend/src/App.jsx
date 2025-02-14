6db5ba64c2d541b4f44928fa759e88e1335e665a
Vinay Kumar <149244101+codes4vinay@users.noreply.github.com>
2025-02-15 04:47:25 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import Store from "./components/Store";
